23509ede3fd8b5a15888af29ba95022310c40ac0
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2023-01-27 16:59:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import streamlit.components.v1 as components
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
@@ -412,3 +413,25 @@ except:
  'An error occured. Check internet connecion and refresh the link.' 
 
 
+takip= """
+<!-- Default Statcounter code for playerrating
+https://euroleagueplayerratings.streamlit.app/ -->
+<script type="text/javascript">
+var sc_project=12842016; 
+var sc_invisible=1; 
+var sc_security="0f9a4c8e"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - Statcounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/12842016/0/0f9a4c8e/1/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
+
+"""
+#st.markdown(takip, unsafe_allow_html=True)  
+components.html(takip,width=200, height=200)  
